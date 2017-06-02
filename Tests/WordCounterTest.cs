73f676d8fb78394a03740ab91c6e8afdd86d7594
nick wise <nickmwise@gmail.com>
2017-06-02 09:07:52 -0700

    Add first test setup. No method written yet.

---
+++ Tests/WordCounterTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace WordCounter
+{
+  public class RepeatCounterTest
+  {
+    [Fact]
+    public void RepeatCounter_GatherInput_equalTo()
+    {
+      // //Arrange
+      // string _input1 = "french";
+      //
+      // // Act
+      // CountRepeats newRepeatCounter = new CountRepeats();
+      //
+      // //Assert
+      // Assert.Equal("french", newRepeatCounter.GetRepeatTrue(input1));
+    }
+  }
+}
