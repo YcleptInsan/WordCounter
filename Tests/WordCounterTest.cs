dd451d9973e64b048ef2316f30445a3f0baf7c44
YcleptInsan <nickmwise@gmail.com>
2017-06-11 09:25:11 -0700

    Refeactoring my test so they actually do something.

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -1,45 +1,47 @@
-// using Xunit;
-// using System.Collections.Generic;
-//
-// namespace WordCounter
-// {
-//   public class RepeatCounterTest
-//   {
-//     // [Fact]
-//     // public void RepeatCounterTest1_GatherInput_equalTo()
-//     // {
-//     //   //Arrange
-//     //   string input1 = "french";
-//     //
-//     //   // Act
-//     //   RepeatCounter newCountRepeats = new RepeatCounter();
-//     //
-//     //   //Assert
-//     //   Assert.Equal(true , newCountRepeats.GetRepeatTrue(input1));
-//     //
-//     [Fact]
-//     public void RepeatCounterTest2_GatherInputs_CompairToString()
-//     {
-//       //Arrange
-//       string input1 = "french";
-//       List<string> input2 = new List<string>();
-//       input2.Add("jenny");
-//       input2.Add("was");
-//       input2.Add("just");
-//       input2.Add("about");
-//       input2.Add("french");
-//
-//       foreach (string match in input2)
-//       {
-//         if (match == input1)
-//         {
-//           System.Console.WriteLine("match");
-//         }
-//         else
-//         {
-//           System.Console.WriteLine("not quite");
-//         }
-//       }
-//     }
-//   }
-// }
+using Xunit;
+using System.Collections.Generic;
+
+namespace WordCounter
+{
+  public class RepeatCounterTest
+  {
+    [Fact]
+    public void RepeatCounterTest1_GatherInputs_equalTo()
+    {
+      //Arrange
+      string input1 = "french";
+			string input2 = "french";
+
+      // Act
+      RepeatCounter newCountRepeats = new RepeatCounter( input1, input2);
+
+      //Assert
+      Assert.Equal(input2 , newCountRepeats.GetInput1());
+
+
+    // [Fact]
+    // public void RepeatCounterTest2_GatherInputs_CompairToString()
+    // {
+    //   //Arrange
+    //   string input1 = "french";
+    //   List<string> input2 = new List<string>();
+    //   input2.Add("jenny");
+    //   input2.Add("was");
+    //   input2.Add("just");
+    //   input2.Add("about");
+    //   input2.Add("french");
+		//
+    //   foreach (string match in input2)
+    //   {
+    //     if (match == input1)
+    //     {
+    //       System.Console.WriteLine("match");
+    //     }
+    //     else
+    //     {
+    //       System.Console.WriteLine("not quite");
+    //     }
+      // }
+    }
+  }
+}
