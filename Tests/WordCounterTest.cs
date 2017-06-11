45449d1e657c68bb03679c6556f2f06c7f9c6f18
YcleptInsan <nickmwise@gmail.com>
2017-06-11 09:33:19 -0700

    add 2nd test passing.

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -17,31 +17,31 @@ namespace WordCounter
 
       //Assert
       Assert.Equal(input2 , newCountRepeats.GetInput1());
-
-
-    // [Fact]
-    // public void RepeatCounterTest2_GatherInputs_CompairToString()
-    // {
-    //   //Arrange
-    //   string input1 = "french";
-    //   List<string> input2 = new List<string>();
-    //   input2.Add("jenny");
-    //   input2.Add("was");
-    //   input2.Add("just");
-    //   input2.Add("about");
-    //   input2.Add("french");
-		//
-    //   foreach (string match in input2)
-    //   {
-    //     if (match == input1)
-    //     {
-    //       System.Console.WriteLine("match");
-    //     }
-    //     else
-    //     {
-    //       System.Console.WriteLine("not quite");
-    //     }
-      // }
     }
+
+		[Fact]
+    public void RepeatCounterTest2_GatherInputs_CompairToString()
+    {
+      //Arrange
+      string input1 = "french";
+      List<string> input2 = new List<string>();
+      input2.Add("jenny");
+      input2.Add("was");
+      input2.Add("just");
+      input2.Add("about");
+      input2.Add("french");
+
+      foreach (string match in input2)
+      {
+        if (match == input1)
+        {
+          System.Console.WriteLine("match");
+        }
+        else
+        {
+          System.Console.WriteLine("not quite");
+        }
+			}
+		}
   }
 }
